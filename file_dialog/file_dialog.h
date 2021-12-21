bc71571545ac71035cab322bbfba67515396483e
Bineet Singh Chadha <bineetchadha@gmail.com>
2021-12-21 13:30:54 +0530

    Add files via upload

---
+++ file_dialog/file_dialog.h
@@ -0,0 +1,32 @@
+
+// file_dialog.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// Cfile_dialogApp:
+// See file_dialog.cpp for the implementation of this class
+//
+
+class Cfile_dialogApp : public CWinApp
+{
+public:
+	Cfile_dialogApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Cfile_dialogApp theApp;
